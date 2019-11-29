0e974cabccecc9c0f34623dce940da53ee883810
pwojczyn <pwojczyn@gmail.com>
2019-11-29 17:42:13 +0000

    1.0.12

--- app.js
+++ app.js
@@ -14,12 +14,18 @@ app.get('/test', function (req, res) {
 });
 
 app.get('/info', function(req, res){
-  res.sendFile('./' + staticFolder + '/info.html', { root: __dirname });
+  res.sendFile(staticFolder + '/info.html', { root: __dirname });
 }); 
 
 app.get('/users/:name', function(req, res) {
     res.send('What is up ' + req.params.name + '!');
     console.log(req);
+});
+
+app.get('/testjson', function(req, res) {
+  var obj = { name: "John", age: 30, city: "New York" };
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(obj));
 });
 
 app.listen(3000, function () {
